7dac88825697d1143bd54167ab945d3b8146e25b
Ruslan Aminev <ruswaserty@yandex.ru>
2017-08-09 17:47:41 +0300

    initialization

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,22 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double init_Kp, double init_Ki, double init_Kd) {
+    //initialize errors
+    p_error = 0.0;
+    i_error = 0.0;
+    d_error = 0.0;
+
+    //initialize coefficients
+    Kp = init_Kp;
+    Ki = init_Ki;
+    Kd = init_Kd;
 }
 
 void PID::UpdateError(double cte) {
+    p_error = cte;
+    
 }
 
 double PID::TotalError() {
 }
-
--- src/PID.h
+++ src/PID.h
@@ -12,7 +12,7 @@ public:
 
   /*
   * Coefficients
-  */ 
+  */
   double Kp;
   double Ki;
   double Kd;
@@ -30,7 +30,7 @@ public:
   /*
   * Initialize PID.
   */
-  void Init(double Kp, double Ki, double Kd);
+  void Init(double init_Kp, double init_Ki, double init_Kd);
 
   /*
   * Update the PID error variables given cross track error.
