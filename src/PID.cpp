cf6451a8d1bea627b03fcaba6fa7bcaac22db0af
Ruslan Aminev <ruswaserty@yandex.ru>
2017-08-12 15:06:46 +0300

    chrono + coef + report

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,7 +10,7 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double init_Kp, double init_Ki, double init_Kd) {
+void PID::Init(double init_Kp, double init_Ki, double init_Kd, long long tick) {
     //initialize errors
     p_error = 0.0;
     i_error = 0.0;
@@ -22,13 +22,17 @@ void PID::Init(double init_Kp, double init_Ki, double init_Kd) {
     Kd = init_Kd;
 
     prev_cte = 0;
+
+    prev_timestamp = tick;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte, long long tick) {
+    double dt = double(tick - prev_timestamp) / 1000000.0; //convert to secods
     p_error = cte;
-    i_error += cte;
-    d_error = cte - prev_cte;
+    i_error += cte * dt;
+    d_error = (cte - prev_cte) / dt;
     prev_cte = cte;
+    prev_timestamp = tick;
 }
 
 double PID::TotalError() {
--- src/PID.h
+++ src/PID.h
@@ -3,46 +3,48 @@
 
 class PID {
 public:
-  /*
-  * Errors
-  */
-  double p_error;
-  double i_error;
-  double d_error;
+    /*
+    * Errors
+    */
+    double p_error;
+    double i_error;
+    double d_error;
 
-  /*
-  * Coefficients
-  */
-  double Kp;
-  double Ki;
-  double Kd;
+    /*
+    * Coefficients
+    */
+    double Kp;
+    double Ki;
+    double Kd;
 
-  double prev_cte;
+    double prev_cte;
 
-  /*
-  * Constructor
-  */
-  PID();
+    long long prev_timestamp;
 
-  /*
-  * Destructor.
-  */
-  virtual ~PID();
+    /*
+    * Constructor
+    */
+    PID();
 
-  /*
-  * Initialize PID.
-  */
-  void Init(double init_Kp, double init_Ki, double init_Kd);
+    /*
+    * Destructor.
+    */
+    virtual ~PID();
 
-  /*
-  * Update the PID error variables given cross track error.
-  */
-  void UpdateError(double cte);
+    /*
+    * Initialize PID.
+    */
+    void Init(double init_Kp, double init_Ki, double init_Kd, long long tick);
 
-  /*
-  * Calculate the total PID error.
-  */
-  double TotalError();
+    /*
+    * Update the PID error variables given cross track error.
+    */
+    void UpdateError(double cte, long long tick);
+
+    /*
+    * Calculate the total PID error.
+    */
+    double TotalError();
 };
 
 #endif /* PID_H */
